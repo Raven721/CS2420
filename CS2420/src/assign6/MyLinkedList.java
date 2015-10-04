c42a1461b2919e1c2b515410667856b4f2536d58
Tim Ellenberger <timellenberger@gmail.com>
2015-10-04 17:23:05 -0600

    Assignment 6: Initial Commit, Ant build for Travis-CI

---
+++ CS2420/src/assign6/MyLinkedList.java
@@ -0,0 +1,192 @@
+package assign6;
+
+import java.util.NoSuchElementException;
+
+/**
+ * A generic and bespoke implementation of a linked list.
+ * 
+ * @author Tim Ellenberger, ellenber
+ * @author Jay Mendez, jaym
+ * @version 10/8/2015
+ * 
+ * @param <E>
+ *            -- the type of elements contained in the list
+ */
+public class MyLinkedList<E> implements List<E>{
+			
+	public MyLinkedList() {
+		
+	}
+	
+	/**
+	 * Inserts the specified element at the beginning of the list.
+	 * O(1) for a doubly-linked list.
+	 */
+	@Override
+	public void addFirst(E element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Inserts the specified element at the end of the list.
+	 * O(1) for a doubly-linked list.
+	 */
+	@Override
+	public void addLast(E o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Inserts the specified element at the specified position in the list.
+	 * Throws IndexOutOfBoundsException if index is out of range.
+	 * O(N) for a doubly-linked list.
+	 */
+	@Override
+	public void add(int index, E element) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Returns the first element in the list.
+	 * Throws NoSuchElementException if the list is empty.
+	 * O(1) for a doubly-linked list.
+	 */
+	@Override
+	public E getFirst() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Returns the last element in the list.
+	 * Throws NoSuchElementException if the list is empty.
+	 * O(1) for a doubly-linked list.
+	 */
+	@Override
+	public E getLast() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Returns the element at the specified position in the list.
+	 * Throws IndexOutOfBoundsException if index is out of range.
+	 * O(N) for a doubly-linked list.
+	 */
+	@Override
+	public E get(int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Returns a randomly-selected element from the list.
+	 * Throws NoSuchElementException if the list is empty.
+	 * O(N) for a doubly-linked list.
+	 */
+	@Override
+	public E getRandom() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Removes and returns the first element from the list.
+	 * Throws NoSuchElementException if the list is empty.
+	 * O(1) for a doubly-linked list.
+	 */
+	@Override
+	public E removeFirst() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Removes and returns the last element from the list.
+	 * Throws NoSuchElementException if the list is empty.
+	 * O(1) for a doubly-linked list.
+	 */
+	@Override
+	public E removeLast() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Removes and returns the element at the specified position in the list.
+	 * Throws IndexOutOfBoundsException if index is out of range.
+	 * O(N) for a doubly-linked list.
+	 */
+	@Override
+	public E remove(int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Returns the index of the first occurrence of the specified element in the list, 
+	 * or -1 if this list does not contain the element.
+	 * O(N) for a doubly-linked list.
+	 */
+	@Override
+	public int indexOf(E element) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * Returns the index of the last occurrence of the specified element in this list, 
+	 * or -1 if this list does not contain the element.
+	 * O(N) for a doubly-linked list.
+	 */
+	@Override
+	public int lastIndexOf(E element) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * Returns the number of elements in this list.
+	 * O(1) for a doubly-linked list.
+	 */
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * Returns true if this collection contains no elements.
+	 * O(1) for a doubly-linked list.
+	 */
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * Removes all of the elements from this list.
+	 * O(1) for a doubly-linked list.
+	 */
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Returns an array containing all of the elements in this list in proper sequence 
+	 * (from first to last element).
+	 * O(N) for a doubly-linked list.
+	 */
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ CS2420/src/assign6/MyLinkedListTester.java
@@ -0,0 +1,38 @@
+package assign6;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A JUnit test class for the MyLinkedList class.
+ * 
+ * @author Tim Ellenberger, ellenber
+ * @author Jay Mendez, jaym
+ * @version 10/8/2015
+ */
+public class MyLinkedListTester {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		System.out.println("Test: Not yet implemented");
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void AllTheMethods() {
+		System.out.println("AllTheMethods: Not yet implemented");
+		fail("None of the methods are implemented yet");
+	}
+
+}
