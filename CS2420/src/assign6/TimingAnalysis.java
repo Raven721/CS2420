a1201ca80e8fbb547ddca2e738ad5f29232e6d50
Tim Ellenberger <timellenberger@gmail.com>
2015-10-05 12:14:23 -0600

    Create TimingAnalysis

---
+++ CS2420/src/assign6/TimingAnalysis.java
@@ -0,0 +1,16 @@
+package assign6;
+
+/**
+ * A JUnit test class for the MyLinkedList class.
+ * 
+ * @author Tim Ellenberger, ellenber
+ * @author Jay Mendez, jaym
+ * @version 10/8/2015
+ */
+public class TimingAnalysis {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
