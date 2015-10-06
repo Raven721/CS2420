38e27bdb2dfd4c002ea4587f26c3f760f0d4ad84
Tim Ellenberger <timellenberger@gmail.com>
2015-10-06 16:28:52 -0600

    Assignment 6: Update Unit Tests

--- CS2420/src/assign6/MyLinkedListTester.java
+++ CS2420/src/assign6/MyLinkedListTester.java
@@ -2,6 +2,8 @@ package assign6;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -14,65 +16,107 @@ import org.junit.rules.ExpectedException;
  * @version 10/8/2015
  */
 public class MyLinkedListTester {
-
+	
+	MyLinkedList<Integer> intList;
+	MyLinkedList<String> stringList;
+	
 	@Rule
 	public final ExpectedException exception = ExpectedException.none();
+	
+	@Before
+	public void setUp() throws Exception {
+		intList = new MyLinkedList<Integer>();
+		stringList = new MyLinkedList<String>();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
 	
 	////////////    int size()   ////////////
 	@Test
 	public void testSize() {
-		MyLinkedList<Integer> list = new MyLinkedList<Integer>();
-
-		list.add(0, 1234);
-		list.addFirst(1);
-		list.addFirst(1);
+		intList.add(0, 1234);
+		intList.addFirst(1);
+		intList.addFirst(1);
 				
-		assertEquals(3, list.size());
+		assertEquals(3, intList.size());
 		
-		list.removeFirst();
-		assertEquals(2, list.size());
+		intList.removeFirst();
+		assertEquals(2, intList.size());
 		
-		list.removeFirst();
-		assertEquals(1, list.size());
+		intList.removeFirst();
+		assertEquals(1, intList.size());
 	}
 	
 	///////////   add(int index, E element)    ////////////
 	@Test
 	public void testAddWithValidInput() {
-		MyLinkedList<Integer> list = new MyLinkedList<Integer>();
-		
-		list.add(0, 5);
-		list.add(1, 5);
-		list.add(2, 5);
-		list.add(3, 5);
-		list.add(4, 5);
-		list.add(5, 5);
-		list.add(4, 5);
-		list.add(3, 5);
-		list.add(2, 5);
-		list.add(1, 5);
-		list.add(0, 5);
+		intList.add(0, 5);
+		intList.add(1, 5);
+		intList.add(2, 5);
+		intList.add(3, 5);
+		intList.add(4, 5);
+		intList.add(5, 5);
+		intList.add(4, 5);
+		intList.add(3, 5);
+		intList.add(2, 5);
+		intList.add(1, 5);
+		intList.add(0, 5);
 		
 		// Exception should be thrown if the input index is < 0 or greater than the size of the LinkedList
 		exception.expect(IndexOutOfBoundsException.class);
-		list.add(-1, 1234);
+		intList.add(-1, 1234);
 		
 		exception.expect(IndexOutOfBoundsException.class);
-		list.add(3, 1234);
+		intList.add(3, 1234);
 	}
 	
 	@Test
 	public void testAddWithInvalidInput() {
-		MyLinkedList<Integer> list = new MyLinkedList<Integer>();
-		
-		list.addFirst(1);
-		list.addFirst(1);
+		intList.addFirst(1);
+		intList.addFirst(1);
 		
 		// Exception should be thrown if the input index is < 0 or greater than the size of the LinkedList
 		exception.expect(IndexOutOfBoundsException.class);
-		list.add(-1, 1234);
+		intList.add(-1, 1234);
 		
 		exception.expect(IndexOutOfBoundsException.class);
-		list.add(3, 1234);
+		intList.add(3, 1234);
+	}
+	
+	/////////////  clear()  //////////////
+	@Test
+	public void testClearWithNonEmptyList() {
+		intList.addFirst(12);
+		intList.addFirst(12);
+		intList.addFirst(12);
+		intList.addFirst(12);
+		
+		assertEquals(4, intList.toArray().length);
+		intList.clear();
+		assertEquals(0, intList.toArray().length);
+	}
+	
+	@Test
+	public void testClearWithEmptyList() {
+		intList.clear();
+		assertEquals(0, intList.toArray().length);
+	}
+	
+	///////////// toArray() //////////////
+	@Test
+	public void testToArrayWithNonEmpty() {
+		intList.addFirst(12);
+		intList.addFirst(12);
+		intList.addFirst(12);
+		intList.addFirst(12);
+		
+		assertEquals(4, intList.toArray().length);
+	}
+	
+	@Test
+	public void testToArrayWithEmptyList() {
+		assertEquals(0, intList.toArray().length);
 	}
 }
