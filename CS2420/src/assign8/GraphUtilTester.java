e799372a8d849f83b662b9b453ccdf1dc10fa5f0
Tim Ellenberger <timellenberger@gmail.com>
2015-10-27 10:08:30 -0600

    Assignment 8: Small update to GraphUtilTester class

--- CS2420/src/assign8/GraphUtilTester.java
+++ CS2420/src/assign8/GraphUtilTester.java
@@ -3,14 +3,12 @@ package assign8;
 import static org.junit.Assert.*;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import assign6.MyLinkedList;
 
 /**
  * A testing suite for the GraphUtil class
@@ -36,7 +34,7 @@ public class GraphUtilTester {
 //	//////////// breadthFirstSearch(String filename, String start, String end) //////////// 
 //	@Test
 //	public void testBreadthFirstSearch() {
-//		List<String> path = GraphUtil.breadthFirstSearch("src\\Tests\\ExampleTests\\examplegraph.dot", "Vertex 1", "Vertex 2");
+//		path = GraphUtil.breadthFirstSearch("src\\Tests\\ExampleTests\\examplegraph.dot", "Vertex 1", "Vertex 2");
 //		System.out.println(path.toString());
 //	}
 //	
@@ -77,7 +75,6 @@ public class GraphUtilTester {
 	public void testTopographicalSortWithExampleGraph6() {
 		exception.expect(UnsupportedOperationException.class);
 		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph6.dot");
-		System.out.println(path.toString());
 	}
 	
 	@Test
@@ -103,14 +100,12 @@ public class GraphUtilTester {
 	@Test
 	public void testBuildGraphFromDotWithDirectedInput() {
 		graph = GraphUtil.buildGraphFromDot("src\\assign8\\Tests\\ExampleTests\\examplegraph.dot");
-		
 		assertTrue(graph.getDirected());
 	}
 	
 	@Test
 	public void testBuildGraphFromDotWithUndirectedInput() {
 		graph = GraphUtil.buildGraphFromDot("src\\assign8\\Tests\\ExampleTests\\examplegraph9.dot");
-		
 		assertFalse(graph.getDirected());
 	}
 }
