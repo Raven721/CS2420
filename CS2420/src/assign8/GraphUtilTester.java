3ae4d9e0224d7a2c30b4d40f1b307bf88b102c02
Tim Ellenberger <timellenberger@gmail.com>
2015-10-27 09:57:33 -0600

    Assignment 8: Update Unit Tests for topological sort

--- CS2420/src/assign8/GraphUtilTester.java
+++ CS2420/src/assign8/GraphUtilTester.java
@@ -3,10 +3,14 @@ package assign8;
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import assign6.MyLinkedList;
 
 /**
  * A testing suite for the GraphUtil class
@@ -17,9 +21,17 @@ import org.junit.rules.ExpectedException;
  * @version 10/29/2015
  */
 public class GraphUtilTester {
+	
+	List<String> path;
+	Graph graph;
 
 	@Rule
 	public final ExpectedException exception = ExpectedException.none();
+	
+	@Before
+	public void setUp() throws Exception {
+		graph = new Graph();
+	}
 	
 //	//////////// breadthFirstSearch(String filename, String start, String end) //////////// 
 //	@Test
@@ -30,26 +42,74 @@ public class GraphUtilTester {
 //	
 	//////////// topologicalSort(String filename) //////////// 
 	@Test
-	public void testTopographical() {
-		List<String> path =  GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph3.dot");
+	public void testTopographicalSortWithExampleGraph() {
+		exception.expect(UnsupportedOperationException.class);
+		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph.dot");
+	}
+	
+	@Test
+	public void testTopographicalSortWithExampleGraph2() {
+		exception.expect(UnsupportedOperationException.class);
+		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph2.dot");
+	}
+	
+	@Test
+	public void testTopographicalSortWithExampleGraph3() {
+		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph3.dot");
+		System.out.println("Example3graph.dot:");
+		System.out.println("\t" + path.toString());
+	}
+
+	@Test
+	public void testTopographicalSortWithExampleGraph4() {
+		exception.expect(UnsupportedOperationException.class);
+		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph4.dot");
+	}
+	
+	@Test
+	public void testTopographicalSortWithExampleGraph5() {
+		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph5.dot");
+		System.out.println("Example5graph.dot:");
+		System.out.println("\t" + path.toString());
+	}
+	
+	@Test
+	public void testTopographicalSortWithExampleGraph6() {
+		exception.expect(UnsupportedOperationException.class);
+		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph6.dot");
+		System.out.println(path.toString());
+	}
+	
+	@Test
+	public void testTopographicalSortWithExampleGraph7() {
+		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph7.dot");
+		System.out.println(path.toString());
+	}
+	
+	@Test
+	public void testTopographicalSortWithExampleGraph8() {
+		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph8.dot");
+		System.out.println(path.toString());
+	}
+	
+	@Test
+	public void testTopographicalSortWithExampleGraph9() {
+		path = GraphUtil.topologicalSort("src\\assign8\\Tests\\ExampleTests\\examplegraph9.dot");
 		System.out.println(path.toString());
 	}
 	
 	//////////// buildGraphFromDot ////////////
 	@Test
 	public void testBuildGraphFromDotWithDirectedInput() {
-		Graph g = new Graph();
-		System.out.println(System.getProperty("user.dir"));
-		g = GraphUtil.buildGraphFromDot("src\\assign8\\Tests\\ExampleTests\\examplegraph.dot");
+		graph = GraphUtil.buildGraphFromDot("src\\assign8\\Tests\\ExampleTests\\examplegraph.dot");
 		
-		assertTrue(g.getDirected());
+		assertTrue(graph.getDirected());
 	}
 	
 	@Test
 	public void testBuildGraphFromDotWithUndirectedInput() {
-		Graph g = new Graph();
-		g = GraphUtil.buildGraphFromDot("src\\assign8\\Tests\\ExampleTests\\examplegraph9.dot");
+		graph = GraphUtil.buildGraphFromDot("src\\assign8\\Tests\\ExampleTests\\examplegraph9.dot");
 		
-		assertFalse(g.getDirected());
+		assertFalse(graph.getDirected());
 	}
 }
