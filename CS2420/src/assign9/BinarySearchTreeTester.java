b169278bfa2089b28675eb857f8b1a5ed63569b0
Tim Ellenberger <timellenberger@gmail.com>
2015-10-31 11:37:10 -0600

    Assignment 9: Add more unit tests

--- CS2420/src/assign9/BinarySearchTreeTester.java
+++ CS2420/src/assign9/BinarySearchTreeTester.java
@@ -2,6 +2,9 @@ package assign9;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,39 +27,84 @@ public class BinarySearchTreeTester {
 	@Before
 	public void setUp() throws Exception {
 		tree = new BinarySearchTree<Integer>();
-	}
-
-	@Test
-	public void testBSTtoArrayList() {
-		// Returning the nodes of an in-order traversal should return a sorted list of the tree items
+		
 		tree.add(12);
 		tree.add(45);
 		tree.add(93);
 		tree.add(32);
+	}
+	
+	@Test
+	public void testBSTaddAll()
+	{
+		// Adding a collection of items should work the same as the regular add method
+		tree = new BinarySearchTree<Integer>();
 
+		Collection<Integer> coll = new ArrayList<Integer>();
+		coll.add(12);
+		coll.add(45);
+		coll.add(93);
+		coll.add(32);
+		
+		tree.addAll(coll);
+		
+		assertTrue(tree.containsAll(coll));
+		assertEquals("[12, 32, 45, 93]", tree.toArrayList().toString());
+	}
+	
+	@Test
+	public void testBSTtoArrayList() {
+		// Returning the nodes of an in-order traversal should return a sorted list of the tree items
 		assertEquals("[12, 32, 45, 93]", tree.toArrayList().toString());
 	}
 
 	@Test
 	public void testBSTFirst() {
 		// The first item in the tree should be the smallest
-		tree.add(12);
-		tree.add(45);
-		tree.add(93);
-		tree.add(32);
-
 		assertEquals((Integer) 12, tree.first());
 	}
 
 	@Test
 	public void testBSTLast() {
 		// The last item in the tree should be the largest
-		tree.add(12);
-		tree.add(45);
-		tree.add(93);
-		tree.add(32);
-
 		assertEquals((Integer) 93, tree.last());
+	}
+	
+	@Test
+	public void testBSTSize() {
+		// The size of this tree should be 4 items
+		assertEquals(4, tree.size());
+	}
+	
+	@Test
+	public void testBSTContains() {
+		// This tree should contain 12, 45, 93 and 32
+		assertTrue(tree.contains(12));
+		assertTrue(tree.contains(45));
+		assertTrue(tree.contains(93));
+		assertTrue(tree.contains(32));
+	}
+	
+	@Test
+	public void testBSTContainsAll() {
+		// This tree should contain 12, 45, 93 and 32
+		Collection<Integer> coll = new ArrayList<Integer>();
+		
+		coll.add(12);
+		coll.add(45);
+		coll.add(93);
+		coll.add(32);
+		
+		assertTrue(tree.containsAll(coll));
+	}
+	
+	@Test
+	public void testBSTisEmpty() {
+		// A tree with zero items (size of 0), should return true when asked if the tree is empty
+		tree = new BinarySearchTree<Integer>();
+		
+		assertEquals(0, tree.size());
+		assertTrue(tree.isEmpty());
 	}
 
 }
