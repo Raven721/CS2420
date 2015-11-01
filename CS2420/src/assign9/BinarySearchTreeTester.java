99a3c2cf0f8b3b90aa7cbb32a378a58f5365cf5e
Tim Ellenberger <timellenberger@gmail.com>
2015-11-01 16:51:52 -0700

    Assignment 9: More unit tests

--- CS2420/src/assign9/BinarySearchTreeTester.java
+++ CS2420/src/assign9/BinarySearchTreeTester.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.NoSuchElementException;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -34,7 +35,7 @@ public class BinarySearchTreeTester {
 		tree.add(32);
 	}
 	
-	//////// add(Type item) /////////
+	//////// addAll(Collection c) /////////
 	@Test
 	public void testBSTaddAll()
 	{
@@ -47,11 +48,76 @@ public class BinarySearchTreeTester {
 		coll.add(93);
 		coll.add(32);
 		
-		tree.addAll(coll);
+		assertTrue(tree.addAll(coll));
 		
 		
 		assertTrue(tree.containsAll(coll));
 		assertEquals("[12, 32, 45, 93]", tree.toArrayList().toString());
+	}
+	
+	//////// removeAll(Collection c) /////////
+	@Test
+	public void testBSTremoveAll()
+	{
+		// Adding a bunch of items, and removing a few of them should pass all tests
+		tree = new BinarySearchTree<Integer>();
+		tree.add(12);
+		tree.add(45);
+		tree.add(93);
+		tree.add(32);
+		tree.add(9);
+		tree.add(2);
+		tree.add(3);
+		tree.add(78);
+		tree.add(112);
+		tree.add(113);
+		tree.add(116);
+		tree.add(31);	
+		tree.add(10);
+		
+		// Verify that these items currently exist in the tree
+		assertTrue(tree.contains(12));
+		assertTrue(tree.contains(45));
+		assertTrue(tree.contains(93));
+		assertTrue(tree.contains(32));
+		
+		// Verify the current size of the tree
+		assertEquals(13, tree.size());
+
+		Collection<Integer> coll = new ArrayList<Integer>();
+		coll.add(12);
+		coll.add(45);
+		coll.add(93);
+		coll.add(32);
+		
+		// Remove the above items from the tree
+		assertTrue(tree.removeAll(coll));
+		
+		// Verify that these items are no longer in the tree
+		assertFalse(tree.contains(12));
+		assertFalse(tree.contains(45));
+		assertFalse(tree.contains(93));
+		assertFalse(tree.contains(32));
+		
+		// Verify the current size of the tree has shrunk by 4 items
+		assertEquals(9, tree.size());
+		
+		assertEquals("[2, 3, 9, 10, 31, 78, 112, 113, 116]", tree.toArrayList().toString());
+	}
+	
+	@Test 
+	public void testBSTremoveAllWithNonExistentItem() {
+		// The current tree does not contain a value of 415
+		tree = new BinarySearchTree<Integer>();
+		
+		Collection<Integer> coll = new ArrayList<Integer>();
+		coll.add(415);
+		
+		// Verify that the tree does not contain a value of 415
+		assertFalse(tree.contains(415));
+		
+		// Should return false because '415' is not in the tree
+		assertFalse(tree.removeAll(coll));
 	}
 	
 	//////// toArrayList() /////////
@@ -67,6 +133,24 @@ public class BinarySearchTreeTester {
 		// The first item in the tree should be the smallest
 		assertEquals((Integer) 12, tree.first());
 	}
+	
+	@Test
+	public void testBSTFirstWithOneNodeTree() {
+		// The first item in this tree should be 12 as it is the only item
+		tree = new BinarySearchTree<Integer>();
+		tree.add(12);
+		
+		assertEquals((Integer)12, tree.first());
+	}
+	
+	@Test
+	public void testBSTFirstWithEmptyTree() {
+		// This test should throw an exception because the tree is empty
+		tree = new BinarySearchTree<Integer>();
+		
+		exception.expect(NoSuchElementException.class);
+		tree.first();
+	}
 
 	//////// last() /////////
 	@Test
@@ -75,11 +159,37 @@ public class BinarySearchTreeTester {
 		assertEquals((Integer) 93, tree.last());
 	}
 	
+	@Test
+	public void testBSTLastWithOneNodeTree() {
+		// The last item in this tree should be 12 as it is the only item
+		tree = new BinarySearchTree<Integer>();
+		tree.add(12);
+				
+		assertEquals((Integer) 12, tree.last());
+	}
+	
+	@Test
+	public void testBSTLastWithEmptyTree() {
+		// This test should throw an exception because the tree is empty
+		tree = new BinarySearchTree<Integer>();
+		
+		exception.expect(NoSuchElementException.class);
+		tree.last();
+	}
+	
 	//////// size() /////////
 	@Test
 	public void testBSTSize() {
 		// The size of this tree should be 4 items
 		assertEquals(4, tree.size());
+	}
+	
+	@Test
+	public void testBSTSizeWithEmptyTree() {
+		// The size of this tree empty tree should be 0 items
+		tree = new BinarySearchTree<Integer>();
+		
+		assertEquals(0, tree.size());
 	}
 	
 	//////// contains(Type item) /////////
@@ -90,6 +200,12 @@ public class BinarySearchTreeTester {
 		assertTrue(tree.contains(45));
 		assertTrue(tree.contains(93));
 		assertTrue(tree.contains(32));
+	}
+	
+	@Test
+	public void testBSTContainsWithNonexistingNode() {
+		// This tree does not contain '415' and should fail when asked if it exists
+		assertFalse(tree.contains(415));
 	}
 	
 	//////// containsAll(Collection c) /////////
@@ -106,14 +222,31 @@ public class BinarySearchTreeTester {
 		assertTrue(tree.containsAll(coll));
 	}
 	
+	@Test
+	public void testBSTContainsAllWithNonexistingNode() {
+		// This tree does not contain '415' and should fail when asked if it exists
+		Collection<Integer> coll = new ArrayList<Integer>();
+		
+		coll.add(415);
+		
+		assertFalse(tree.containsAll(coll));
+	}
+	
 	//////// isEmpty() /////////
 	@Test
-	public void testBSTisEmpty() {
+	public void testBSTisEmptyWithEmptyTree() {
 		// A tree with zero items (size of 0), should return true when asked if the tree is empty
 		tree = new BinarySearchTree<Integer>();
 		
 		assertEquals(0, tree.size());
 		assertTrue(tree.isEmpty());
+	}
+	
+	@Test
+	public void testBSTisEmptyWithNonTree() {
+		// A tree with four nodes should return false when asked if empty
+		assertEquals(4, tree.size());
+		assertFalse(tree.isEmpty());
 	}
 	
 	//////// generateDotFromBST(String filename)  /////////
